a3cf0a8f43fc4e51028eaaf3f647a24030dd85f1
gengyi <junjunxu92@gmail.com>
2025-08-03 17:47:56 +0800

    change

--- pages/concat.vue
+++ pages/concat.vue
@@ -6,95 +6,77 @@
         class="about_view_section_img"
       />
       <div class="about_view_section_box">
-        <h4 class="about_view_section_title">{{t('contact.title')}}</h4>
-        <div v-if='isMobile'>
+        <div class="about_view_section_title" v-if='isMobile'>
+          {{t('contact.title')}}
+        </div>
+        <div class="about_view_section_title" v-else>
+          {{t('contact.title')}}
+          <p class="about_view_section_title_vip">
+            <img
+              src="/contact/icon.png"
+              class="about_view_section_title_vip_img"
+            />{{t('contact.notice')}}&nbsp;public@antoralife.com
+          </p>
+        </div>
+        <!-- <div v-if='isMobile'>
           <p class="about_view_main_vip">
             <img
               src="/contact/email.png"
               class="about_view_main_vip_img"
             />&nbsp;public@antoralife.com
-            <!-- {{t('contact.VIP')}} -->
-            <!-- 邮箱 -->
           </p>
-          <!-- <p class="about_view_main_vip">public@antoralife.com</p> -->
         </div>
         <p class="about_view_main_vip" v-else>
-          <!-- {{t('contact.VIP')}}&nbsp; -->
           <img
             src="/contact/email.png"
             class="about_view_main_vip_img"
           />&nbsp;
-          <!-- 邮箱 -->public@antoralife.com
-        </p>
+          public@antoralife.com
+        </p> -->
         <div class="about_view_main_contact">
-          <img
-            src="/contact/phone.png"
-            class="about_view_main_contact_img"
-          />
-          <img
-            src="/contact/tg.png"
-            class="about_view_main_contact_img"
-          />
-          <img
-            src="/contact/wechat.png"
-            class="about_view_main_contact_img"
-          />
+          <div class="about_view_main_contact_img">
+            <img
+              src="/contact/phone.png"
+              class="about_view_main_contact_img_icon"
+            />
+            <p>Whats App</p>
+          </div>
+          <div class="about_view_main_contact_img">
+            <img
+              src="/contact/tg.png"
+              class="about_view_main_contact_img_icon"
+            />
+            <p>Telegram</p>
+          </div>
+          <div class="about_view_main_contact_img">
+            <img
+              src="/contact/wechat.png"
+              class="about_view_main_contact_img_icon"
+            />
+            <p>Wechat</p>
+          </div>
         </div>
-        <p class="about_view_main_tip">{{t('contact.tip')}}</p>
+        <div class="about_view_main_info">
+          <div class="about_view_main_info_item">
+            <div class="about_view_main_info_item_name">厦门安桐进出口有限公司</div>
+            <div class="about_view_main_info_item_desc">+86 186 0692 2333(中国大陆)</div>
+          </div>
+          <div class="about_view_main_info_item">
+            <div class="about_view_main_info_item_name">安桐有限公司</div>
+            <div class="about_view_main_info_item_desc">+852 770 57 929(中国香港)</div>
+          </div>
+          <div class="about_view_main_info_item">
+            <div class="about_view_main_info_item_name">安桐柬埔寨进出口有限公司</div>
+            <div class="about_view_main_info_item_desc">+855 188 56 9999(柬埔寨)</div>
+          </div>
+        </div>
       </div>
+
+      <img
+        src="/contact/map.png"
+        class="about_view_section_img"
+      />
     </div>
-    <div class="about_view_line"></div>
-    <!-- <div class="about_view_main">
-      <h4 class="about_view_main_title">{{t('contact.subtitle')}}</h4>
-      <div class="about_view_main_box">
-        <p class="about_view_main_subtitle">ANTORA COMPANY LIMITED</p>
-        <p class="about_view_main_desc">安桐（香港）有限公司</p>
-        <p class="about_view_main_tips">UNIT 2208, 22/F YAN'S TOWER, 25-27 WONG CHUK HANG ROAD, WONG CHUK HANG, HK.</p>
-      </div>
-      <div class="about_view_main_box">
-        <p class="about_view_main_subtitle">XIAMEN ANTORA IMPORT AND EXPORT CO., LTD.</p>
-        <p class="about_view_main_desc">厦门安桐进出口有限公司</p>
-        <p class="about_view_main_tips">Unit 604, No. 291, Bingcheng Road, Huli District, Xiamen City, Fujian Province, China.</p>
-      </div>
-      <div class="about_view_main_box">
-        <p class="about_view_main_subtitle">ANTORA CAMBODIAN IMPORT EXPORT CO., LTD.</p>
-        <p class="about_view_main_desc">安桐柬埔寨进出口有限公司</p>
-        <p class="about_view_main_tips">#263, St NR6, Phum Kien Khleang, Sangkat Prek leap, Khan Chroy Chongva, Phnom Penh, Cambodia.</p>
-      </div>
-      <div v-if='isMobile'>
-        <p class="about_view_main_vip">
-          <img
-            src="/contact/tel.png"
-            class="about_view_main_vip_img"
-          />&nbsp;
-          {{t('contact.VIP')}}
-        </p>
-        <p class="about_view_main_vip">+86 186 0692 2333&nbsp;&nbsp;+855 188 56 9999</p>
-      </div>
-      <p class="about_view_main_vip" v-else>
-        {{t('contact.VIP')}}&nbsp;
-        <img
-          src="/contact/tel.png"
-          class="about_view_main_vip_img"
-        />
-        &nbsp;&nbsp;+86 186 0692 2333&nbsp;&nbsp;+855 188 56 9999
-      </p>
-      <div class="about_view_main_contact">
-        <img
-          src="/contact/phone.png"
-          class="about_view_main_contact_img"
-        />
-        <img
-          src="/contact/tg.png"
-          class="about_view_main_contact_img"
-        />
-        <img
-          src="/contact/wechat.png"
-          class="about_view_main_contact_img"
-        />
-      </div>
-      <p class="about_view_main_tip">{{t('contact.tip')}}</p>
-    </div> -->
   </div>
 </template>
 <script setup>
